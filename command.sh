f5e05774856d7c57b006e126a8c2c980551cd11f
Carlos Urriola <carlos@zignos.com>
2022-05-13 14:47:40 -0500

    working RDS command

--- command.sh
+++ command.sh
@@ -1,19 +1,19 @@
 # zip dependencies from virtualenv and source files
 rm src/lambda.zip
-cd env/lib/python3.6/site-packages/
+cd env/lib/python3.8/site-packages/
 zip -r9 ../../../../src/lambda.zip *
 cd ../../../../src/
 zip -g lambda.zip sql_queries.py lambda_function.py database.cfg
 cd ..
 
 # package cloudformation
-aws cloudformation package --s3-bucket bucket-name-for-package \
+aws cloudformation package --s3-bucket devopsteststore \
     --template-file template.yaml \
-    --output-template-file gen/template-generated.yaml
+    --output-template-file template-generated.yaml
 
 # deploy cloudformation
 aws cloudformation deploy \
-    --template-file gen/template-generated.yaml \
+    --template-file template-generated.yaml \
     --stack-name S3toRdsLambda \
     --capabilities CAPABILITY_IAM
 
